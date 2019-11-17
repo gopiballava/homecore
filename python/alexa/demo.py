fe68bede22c7ad36e02007b9744134768dc22103
Gopiballava <gopiballava@gmail.com>
2019-11-17 00:42:34 -0500

    Say the message properly

--- python/alexa/demo.py
+++ python/alexa/demo.py
@@ -6,7 +6,7 @@ name_map = {}
 @app.launch
 def launch_handler(*args):
     print("Args: {}".format(str(args)))
-    return alexandra.reprompt("Hello, I'm a raspberry pi velcroed onto your freezer.")
+    return alexandra.respond("Hello, I'm a raspberry pi velcroed onto your freezer.")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8050, debug=True)
