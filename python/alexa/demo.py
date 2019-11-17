71277c0d9e3c72c88f0b67f9791eb393af5d3d9a
Gopiballava <gopiballava@gmail.com>
2019-11-17 00:41:09 -0500

    Added demo

---
+++ python/alexa/demo.py
@@ -0,0 +1,13 @@
+import alexandra
+
+app = alexandra.Application()
+name_map = {}
+
+@app.launch
+def launch_handler(*args):
+    print("Args: {}".format(str(args)))
+    return alexandra.reprompt("Hello, I'm a raspberry pi velcroed onto your freezer.")
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8050, debug=True)
+
