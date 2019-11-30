b7cc9471a8dc0aee3bfb80533f0beb8a43156486
Gopiballava <gopiballava@gmail.com>
2019-11-30 17:34:49 -0500

    Works correctly, ish

--- python/server/measurements.py
+++ python/server/measurements.py
@@ -8,11 +8,16 @@ class Measurements(object):
 		
 	@cherrypy.expose
 	def reading(self, type, id, value):
-		print(type, id, value)
+		temp_c = float(value) / 100
+		temp_f = (( 9./5) * temp_c) + 32
+		print("====>    {}     {:.1f}C   {:.1f}F".format(id, temp_c, temp_f))
 
 
 def main(argv):
 	cherrypy.config.update( {'server.socket_host': '0.0.0.0',
-							'server.socket_port':  8242,
+							'server.socket_port':  8192,
 							})
 	cherrypy.quickstart(Measurements())
+
+if __name__ == '__main__':
+    main([])
