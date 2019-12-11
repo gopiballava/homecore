f22c47bc3cce7b8aa506c8a119371349d3b94c9b
Gopiballava Flaherty <gopiballava@gmail.com>
2019-12-10 20:04:41 -0800

    add stub

--- python/alexa/demo.py
+++ python/alexa/demo.py
@@ -4,8 +4,8 @@ app = alexandra.Application()
 name_map = {}
 
 @app.launch
-def launch_handler(*args):
-    print("Args: {}".format(str(args)))
+def launch_handler(session):
+    print("Session: {}".format(str(session)))
     return alexandra.respond("Hello, I'm a raspberry pi velcroed onto your freezer.")
 
 if __name__ == '__main__':
---
+++ python/lru/lru.py
@@ -0,0 +1,16 @@
+
+
+
+class doubleLink(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insertAtHead(self, val):
+        pass
+    
+    def moveToHead(self, obj):
+        pass
+    
+    def popFromTail(self):
+        pass
