cce30caee5ff352975f0f22b3ee9a6e5321e4085
Gopiballava Flaherty <gopiballava@gmail.com>
2019-12-24 12:20:21 -0500

    Add a persistent object

--- python/zodb/basic_test.py
+++ python/zodb/basic_test.py
@@ -1,8 +1,24 @@
 
 import ZODB, ZODB.FileStorage
+import persistent
+from BTrees.OOBTree import TreeSet
+
+class Book(persistent.Persistent):
+
+   def __init__(self, title):
+       self.title = title
+       self.authors = TreeSet()
+
+   def add_author(self, author):
+       self.authors.add(author)
+
 
 def test_storage():
     #storage = ZODB.FileStorage.FileStorage('mydata.fs')
-    db = ZODB.DB(None)
+    storage = None
+    db = ZODB.DB(storage)
     connection = db.open()
     root = connection.root
+    book = Book("ZODB")
+    connection.add(book)
+    
