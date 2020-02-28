6fc40a738008ce0be9cef19d92a427648d36fe60
Gopiballava Flaherty <gopiballava@gmail.com>
2020-02-27 22:12:56 -0500

    zope account

--- python/zodb/basic_test.py
+++ python/zodb/basic_test.py
@@ -6,3 +6,17 @@ def test_storage():
     db = ZODB.DB(None)
     connection = db.open()
     root = connection.root
+
+import persistent
+
+class Account(persistent.Persistent):
+
+    def __init__(self):
+        self.balance = 0.0
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def cash(self, amount):
+        assert amount < self.balance
+        self.balance -= amount
