13b417e4c7f46d70e2a0d4780819436b3f964dea
gopiballava <gopiballava@gmail.com>
2020-07-05 17:12:25 -0400

    WiP

--- python/zodb/basic_test.py
+++ python/zodb/basic_test.py
@@ -4,22 +4,28 @@ import sys
 import ZODB
 import ZODB.FileStorage
 import persistent
-import persistent.list
+from persistent.list import PersistentList
+from persistent.mapping import PersistentMapping
 import transaction
 from BTrees.OOBTree import TreeSet, BTree
 
 
 # @total_ordering
 class Node(persistent.Persistent):
+    from_edges_allowed = set()
+    to_edges_allowed = set()
 
     def __init__(self, title):
         self.title = title
-        self.from_edges = TreeSet()
-#         self.from_edges = persistent.list.PersistentList()
-        self.to_edges = persistent.list.PersistentList()
+        self.from_edges = PersistentMapping()
+        self.to_edges = PersistentMapping()
+        if issubclass(self.__class__, ConnectableNode):
+            self.from_edges_allowed = set(self.from_edges_allowed)
+            self.from_edges_allowed.add('IsPartOfHouse')
+        
 
-    def add_from(self, edge):
-        self.from_edges.add(edge)
+#     def add_from(self, edge):
+#         self.from_edges.add(edge)
 #     def __cmp__(self, other):
 #         print("CALLED")
 #         return cmp(self.title, other.title)
@@ -29,19 +35,65 @@ class Node(persistent.Persistent):
 #         return self.__hash__() < other.__hash__()
 
 
-class ESPNode(Node):
+class LogicalHouse(Node):
+    from_edges_allowed = set()
+    to_edges_allowed = set(('IsPartOfHouse', ))
+
+    def create_connected_node(self, node_type: str, node_name: str):
+        new_node = globals()[node_type](node_name)
+        IsPartOfHouse(new_node, self)
+        return new_node
+    
+    def get_connected_nodes(self):
+        return [edge.from_node for edge in self.to_edges['IsPartOfHouse']]
+
+class ConnectableNode(Node):
+    from_edges_allowed = set(('IsPartOfHouse',))
+    to_edges_allowed = set()
+        
+
+class ESPNode(ConnectableNode):
     def get_connected_sensors(self):
         pass
 
+
+class GPIOConnector(ConnectableNode):
+    from_edges_allowed = set(('IsPartOfHouse', 'IsPluggedInToConnector'))
+    to_edges_allowed = set(('IsPluggedInToConnector',))
+
+class PhysicalSensor(ConnectableNode):
+    pass
+
+class WaterFloatSensor(PhysicalSensor):
+    from_edges_allowed = set(('IsPartOfHouse', 'IsPluggedInToConnector'))
+    to_edges_allowed = set()
+
+
+def add_object_to_mapping(obj, mapping, class_names):
+    for class_name in class_names:
+        if issubclass(obj.__class__, globals()[class_name]):
+            if class_name in mapping:
+                object_list = mapping[class_name]
+            else:
+                object_list = PersistentList()
+                mapping[class_name] = object_list
+            object_list.append(obj)
+            return
+    raise RuntimeError('Not allowed to add {} to {}'.format(obj, class_names))
+
+            
 # @total_ordering
 class Edge(persistent.Persistent):
     def __init__(self, from_node, to_node):
+        print("Adding edge", from_node, to_node)
         self.from_node = from_node
         self.to_node = to_node
-        print("f,t", from_node, from_node.from_edges, self)
-        self.from_node.from_edges.add(self)
-        print(self.from_node.from_edges)
-        self.to_node.to_edges.append(self)
+#         print("f,t", from_node, from_node.from_edges, self)
+#         print(self.from_node.from_edges)
+        add_object_to_mapping(self, self.from_node.from_edges, self.from_node.from_edges_allowed)
+        add_object_to_mapping(self, self.to_node.to_edges, self.to_node.to_edges_allowed)
+#         self.from_node.from_edges.append(self)
+#         self.to_node.to_edges.append(self)
     
     def __cmp__(self, other):
         print("EDGE")
@@ -53,7 +105,16 @@ class Edge(persistent.Persistent):
     def desc(self):
         print("I'm an EDGE")
 
-#   def connect_nodes(self, from, to):
+
+class IsPartOfHouse(Edge):
+    pass
+
+class HasAttachedConnector(Edge):
+    pass
+
+class IsPluggedInToConnector(Edge):
+    pass
+
 
 
 def make_storage():
@@ -75,6 +136,29 @@ def cli_write(connection=None):
     if connection is None:
         connection = make_storage()
     root = connection.root
+    root.house = LogicalHouse('beechview_house')
+    beechview = root.house
+
+    esp = root.house.create_connected_node('ESPNode', "esp8266")
+    water_sensor = beechview.create_connected_node('WaterFloatSensor', 'water_sensor')
+    water_gpio = beechview.create_connected_node('GPIOConnector', 'water_gpio')
+    e = IsPluggedInToConnector(water_sensor, water_gpio)
+#     HasAttachedConnector(esp, water_gpio)
+    for node in beechview.get_connected_nodes():
+        print(node)
+        print('  to:   {}'.format(node.to_edges))
+        print('  from: {}'.format(node.from_edges))
+    print(esp.from_edges)
+    print("Is sub", issubclass( beechview.__class__, globals()['Node']))
+    print("Is sub", issubclass(LogicalHouse, globals()['Node']))
+    print("Is sub", issubclass(Node, globals()['Node']))
+#     root.nodes['wtf'] = e
+#     esp.from_edges.append(water_sensor)
+    transaction.commit()
+
+def test_write_old():
+    connection = make_storage()
+    root = connection.root
     root.nodes = BTree()
 
     esp = ESPNode("esp8266")
@@ -91,6 +175,21 @@ def cli_write(connection=None):
 def cli_read(connection=None):
     if connection is None:
         connection = make_storage()
+    root = connection.root
+    beechview = root.house
+    for node in beechview.get_connected_nodes():
+        print(node)
+        print('  to: SOMEONE is VERB {} to ME '.format(node.to_edges))
+        print('  from; This/I am : {}'.format(node.from_edges))
+    return
+    print(root.nodes['first_esp'])
+    for edge in root.nodes['first_esp'].from_edges:
+        print("  Edge: {} to {}".format(edge, edge.to_node))
+#     print(dir(connection))
+#     print(connection.book)
+
+def test_read_old():
+    connection = make_storage()
     root = connection.root
     print(root.nodes['first_esp'])
     for edge in root.nodes['first_esp'].from_edges:
