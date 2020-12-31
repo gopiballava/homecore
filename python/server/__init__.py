004d3de6e2a4d73ab52ea3003c95fee89de9a296
Gopiballava Flaherty <gopiballava@gmail.com>
2020-12-30 21:51:34 -0500

    misc

---
+++ python/server/__init__.py
@@ -0,0 +1,20 @@
+# Files
+bootstrap.py
+* try...except loads first zipfile, then second? third?
+
+boot.zip
+* Infrequently replaced, globally shared
+* reaches out to server, asks what version(s) of app and boot we should have
+* replaces app.zip with new version(?)
+* can also just load a zipfile directly into memory
+
+emergency_boot.zip:
+* rarely replaced backup
+
+app.zip
+* customized application code
+* has some sort of UID hash amd version
+
+/app/get: 
+    mac_address
+
--- python/zodb/basic_test.py
+++ python/zodb/basic_test.py
@@ -4,28 +4,21 @@ import sys
 import ZODB
 import ZODB.FileStorage
 import persistent
-from persistent.list import PersistentList
-from persistent.mapping import PersistentMapping
+import persistent.list
 import transaction
 from BTrees.OOBTree import TreeSet, BTree
 
-
 # @total_ordering
 class Node(persistent.Persistent):
-    from_edges_allowed = set()
-    to_edges_allowed = set()
 
     def __init__(self, title):
         self.title = title
-        self.from_edges = PersistentMapping()
-        self.to_edges = PersistentMapping()
-        if issubclass(self.__class__, ConnectableNode):
-            self.from_edges_allowed = set(self.from_edges_allowed)
-            self.from_edges_allowed.add('IsPartOfHouse')
-        
+        self.from_edges = TreeSet()
+#         self.from_edges = persistent.list.PersistentList()
+        self.to_edges = persistent.list.PersistentList()
 
-#     def add_from(self, edge):
-#         self.from_edges.add(edge)
+    def add_from(self, edge):
+        self.from_edges.add(edge)
 #     def __cmp__(self, other):
 #         print("CALLED")
 #         return cmp(self.title, other.title)
@@ -34,66 +27,19 @@ class Node(persistent.Persistent):
 #     def __lt__(self, other):
 #         return self.__hash__() < other.__hash__()
 
-
-class LogicalHouse(Node):
-    from_edges_allowed = set()
-    to_edges_allowed = set(('IsPartOfHouse', ))
-
-    def create_connected_node(self, node_type: str, node_name: str):
-        new_node = globals()[node_type](node_name)
-        IsPartOfHouse(new_node, self)
-        return new_node
-    
-    def get_connected_nodes(self):
-        return [edge.from_node for edge in self.to_edges['IsPartOfHouse']]
-
-class ConnectableNode(Node):
-    from_edges_allowed = set(('IsPartOfHouse',))
-    to_edges_allowed = set()
-        
-
-class ESPNode(ConnectableNode):
+class ESPNode(Node):
     def get_connected_sensors(self):
         pass
 
-
-class GPIOConnector(ConnectableNode):
-    from_edges_allowed = set(('IsPartOfHouse', 'IsPluggedInToConnector'))
-    to_edges_allowed = set(('IsPluggedInToConnector',))
-
-class PhysicalSensor(ConnectableNode):
-    pass
-
-class WaterFloatSensor(PhysicalSensor):
-    from_edges_allowed = set(('IsPartOfHouse', 'IsPluggedInToConnector'))
-    to_edges_allowed = set()
-
-
-def add_object_to_mapping(obj, mapping, class_names):
-    for class_name in class_names:
-        if issubclass(obj.__class__, globals()[class_name]):
-            if class_name in mapping:
-                object_list = mapping[class_name]
-            else:
-                object_list = PersistentList()
-                mapping[class_name] = object_list
-            object_list.append(obj)
-            return
-    raise RuntimeError('Not allowed to add {} to {}'.format(obj, class_names))
-
-            
 # @total_ordering
 class Edge(persistent.Persistent):
     def __init__(self, from_node, to_node):
-        print("Adding edge", from_node, to_node)
         self.from_node = from_node
         self.to_node = to_node
-#         print("f,t", from_node, from_node.from_edges, self)
-#         print(self.from_node.from_edges)
-        add_object_to_mapping(self, self.from_node.from_edges, self.from_node.from_edges_allowed)
-        add_object_to_mapping(self, self.to_node.to_edges, self.to_node.to_edges_allowed)
-#         self.from_node.from_edges.append(self)
-#         self.to_node.to_edges.append(self)
+        print("f,t", from_node, from_node.from_edges, self)
+        self.from_node.from_edges.add(self)
+        print(self.from_node.from_edges)
+        self.to_node.to_edges.append(self)
     
     def __cmp__(self, other):
         print("EDGE")
@@ -105,17 +51,7 @@ class Edge(persistent.Persistent):
     def desc(self):
         print("I'm an EDGE")
 
-
-class IsPartOfHouse(Edge):
-    pass
-
-class HasAttachedConnector(Edge):
-    pass
-
-class IsPluggedInToConnector(Edge):
-    pass
-
-
+#   def connect_nodes(self, from, to):
 
 def make_storage():
     storage = ZODB.FileStorage.FileStorage('mydata.fs')
@@ -124,39 +60,7 @@ def make_storage():
     connection = db.open()
     return connection
 
-
-def test_rw():
-    db = ZODB.DB(None)
-    connection = db.open()
-    cli_write(connection=connection)
-    cli_read(connection=connection)
-
-
-def cli_write(connection=None):
-    if connection is None:
-        connection = make_storage()
-    root = connection.root
-    root.house = LogicalHouse('beechview_house')
-    beechview = root.house
-
-    esp = root.house.create_connected_node('ESPNode', "esp8266")
-    water_sensor = beechview.create_connected_node('WaterFloatSensor', 'water_sensor')
-    water_gpio = beechview.create_connected_node('GPIOConnector', 'water_gpio')
-    e = IsPluggedInToConnector(water_sensor, water_gpio)
-#     HasAttachedConnector(esp, water_gpio)
-    for node in beechview.get_connected_nodes():
-        print(node)
-        print('  to:   {}'.format(node.to_edges))
-        print('  from: {}'.format(node.from_edges))
-    print(esp.from_edges)
-    print("Is sub", issubclass( beechview.__class__, globals()['Node']))
-    print("Is sub", issubclass(LogicalHouse, globals()['Node']))
-    print("Is sub", issubclass(Node, globals()['Node']))
-#     root.nodes['wtf'] = e
-#     esp.from_edges.append(water_sensor)
-    transaction.commit()
-
-def test_write_old():
+def test_write():
     connection = make_storage()
     root = connection.root
     root.nodes = BTree()
@@ -171,24 +75,7 @@ def test_write_old():
 #     esp.from_edges.append(water_sensor)
     transaction.commit()
 
-
-def cli_read(connection=None):
-    if connection is None:
-        connection = make_storage()
-    root = connection.root
-    beechview = root.house
-    for node in beechview.get_connected_nodes():
-        print(node)
-        print('  to: SOMEONE is VERB {} to ME '.format(node.to_edges))
-        print('  from; This/I am : {}'.format(node.from_edges))
-    return
-    print(root.nodes['first_esp'])
-    for edge in root.nodes['first_esp'].from_edges:
-        print("  Edge: {} to {}".format(edge, edge.to_node))
-#     print(dir(connection))
-#     print(connection.book)
-
-def test_read_old():
+def test_read():
     connection = make_storage()
     root = connection.root
     print(root.nodes['first_esp'])
@@ -196,7 +83,6 @@ def test_read_old():
         print("  Edge: {} to {}".format(edge, edge.to_node))
 #     print(dir(connection))
 #     print(connection.book)
-
 
 class Account(persistent.Persistent):
 
@@ -213,9 +99,9 @@ class Account(persistent.Persistent):
 
 if __name__ == '__main__':
     if sys.argv[1] == 'w':
-        cli_write()
+        test_write()
     elif sys.argv[1] == 'r':
-        cli_read()
+        test_read()
     else:
         print("Usage: {} w | r".format(sys.argv[0]))
         print("w writes data to the store on disk, r reads it.")
