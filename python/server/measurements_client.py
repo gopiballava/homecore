96147e28c2f5acbec67c0aef4a15b47b6dfb5242
Gopiballava Flaherty <gopiballava@gmail.com>
2021-03-20 18:31:40 -0400

    typo

--- python/server/measurements_client.py
+++ python/server/measurements_client.py
@@ -13,7 +13,7 @@ class MeasurementsClient:
     def _send_reading(self, reading_type, oid, value):
         response = urequests.get('http://{}:{}/new_reading/{}/{}/{}'.format(SERVER_IP, SERVER_PORT, reading_type, oid, value))
         if response.status_code != 200:
-            print('http error code {}, reason {}'.format(response.status_code, response.reason)
+            print('http error code {}, reason {}'.format(response.status_code, response.reason))
 
 
 class VoltageMeasurement(MeasurementClient):
